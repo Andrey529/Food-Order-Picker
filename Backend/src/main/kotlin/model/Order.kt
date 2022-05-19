8405dc46fe869f75572b1a84260e431b4e766ced
Andrey <75383288+Andrey529@users.noreply.github.com>
2022-05-19 19:09:48 +0300

    fix(backend): fix order properties

--- Backend/src/main/kotlin/model/Order.kt
+++ Backend/src/main/kotlin/model/Order.kt
@@ -9,8 +9,8 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Order(
     @Serializable(with = UUIDSerializer::class) val userId: UUID,
-    @Serializable(with = UUIDSerializer::class) val orderId: UUID?,
-    val burgers: MutableList<Burger>,
-    val drinks: MutableList<Drink>,
-    val desserts: MutableList<Dessert>
+    @Serializable(with = UUIDSerializer::class) val orderId: UUID,
+    val burgers: MutableList<Burger> = mutableListOf(),
+    val drinks: MutableList<Drink> = mutableListOf(),
+    val desserts: MutableList<Dessert> = mutableListOf()
 )
