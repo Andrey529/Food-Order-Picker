2a2c2313586fc089b41ba111f5a6a53e13083f1d
Andrey <75383288+Andrey529@users.noreply.github.com>
2022-05-20 20:21:26 +0300

    Write test endpoint (#12)
    * feat(backend): add first http trigger function addBurger with post request
    
    * Add or update the Azure App Service build and deployment workflow config
    
    * fix(backend): fix path to deploy functions
    
    * fix(backend): replace autorization level from function to anonymous
    
    * fix(backend): add body to request response
    
    * fix(backend): change response http status
    
    * fix(backend): change check for request body and response status
    
    * fix(backend): replace property isEmpty to function isEmpty()
    
    * fix(backend): fix check request.body.isEmpty()
    
    * fix(backend): fix check request.body.isEmpty()
    
    * fix(backend): fix check request.body.isEmpty()
    
    * fix(backend): fix check request.body.isEmpty()
    
    * fix(backend): fix check request.body.isEmpty()
    
    * refactored(backend): refactored code
    
    * fix(deploy pipline): delete trigger on push branch

---
+++ Backend/src/main/kotlin/controllers/AddBurger.kt
@@ -0,0 +1,40 @@
+package controllers
+
+import com.microsoft.azure.functions.annotation.HttpTrigger
+import com.microsoft.azure.functions.HttpMethod
+import com.microsoft.azure.functions.annotation.AuthorizationLevel
+import com.microsoft.azure.functions.HttpRequestMessage
+import com.microsoft.azure.functions.ExecutionContext
+import com.microsoft.azure.functions.HttpResponseMessage
+import com.microsoft.azure.functions.HttpStatus
+import com.microsoft.azure.functions.annotation.FunctionName
+import java.util.*
+
+class AddBurger {
+    @FunctionName("AddBurger")
+    fun run(
+        @HttpTrigger(name = "req",
+            methods = [HttpMethod.POST],
+            authLevel = AuthorizationLevel.ANONYMOUS,
+            route = "burger"
+        ) request: HttpRequestMessage<Optional<String?>>,
+        context: ExecutionContext,
+    ): HttpResponseMessage {
+        context.logger.info("AddBurger HTTP trigger function invoked with post method.")
+
+        val requestBody = request.body
+
+        return if ((requestBody == null) || !requestBody.isPresent) {
+            context.logger.info("Request body is empty.")
+            request.createResponseBuilder(HttpStatus.BAD_REQUEST)
+                .body("Not a valid request").build()
+        } else {
+
+//            TODO("Deserialize json with burger and add it to CosmosDB")
+
+
+            context.logger.info("Created new burger.")
+            request.createResponseBuilder(HttpStatus.OK).body("Okay").build()
+        }
+    }
+}
