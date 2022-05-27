80e64b771f27fc16a1ea19292bb81807e9ea8fad
Andrey <75383288+Andrey529@users.noreply.github.com>
2022-05-27 22:14:32 +0300

    fix(backend): remove partionKey()

--- Backend/src/main/kotlin/controllers/AddBurger.kt
+++ Backend/src/main/kotlin/controllers/AddBurger.kt
@@ -58,7 +58,7 @@ class AddBurger {
             context.logger.info("Created cosmos item request options.")
 
             val item: CosmosItemResponse<Burger> =
-                container!!.createItem(burger, PartitionKey("burger"), cosmosItemRequestOptions)
+                container!!.createItem(burger, cosmosItemRequestOptions)
 
             context.logger.info("Created item with request charge of ${item.requestCharge} within duration ${item.duration}");
 
