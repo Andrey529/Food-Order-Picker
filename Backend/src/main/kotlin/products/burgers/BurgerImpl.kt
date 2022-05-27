f996634720a74107efb80925faabe952ace22f11
Andrey <75383288+Andrey529@users.noreply.github.com>
2022-05-27 22:01:34 +0300

    feat(backend): add id property to CheeseBurger

--- Backend/src/main/kotlin/products/burgers/BurgerImpl.kt
+++ Backend/src/main/kotlin/products/burgers/BurgerImpl.kt
@@ -1,7 +1,11 @@
 package products.burgers
 
+import kotlinx.serialization.Serializable
+import model.UUIDSerializer
+import java.util.*
+
 @kotlinx.serialization.Serializable
-data class CheeseBurger(override val price: Int, override val size: BurgerSize) : Burger {
+data class CheeseBurger(override val price: Int, override val size: BurgerSize, @Serializable(with = UUIDSerializer::class) val id: UUID = UUID.randomUUID()) : Burger {
     init {
         require(price > 0) { "Price must be greater than 0" }
     }
