cc1b35f0045fc9554455e40cb7f5f9cfe8688c1a
Andrey <75383288+Andrey529@users.noreply.github.com>
2022-05-31 23:46:38 +0300

    fix(backend): rename orderId to id

--- Backend/src/main/kotlin/model/Order.kt
+++ Backend/src/main/kotlin/model/Order.kt
@@ -1,5 +1,6 @@
 package model
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import kotlinx.serialization.Serializable
 import products.burgers.Burger
 import products.desserts.Dessert
@@ -7,8 +8,8 @@ import products.drinks.Drink
 
 @Serializable
 data class Order(
-    var id: String?,
-    val burgers: MutableList<Burger> = mutableListOf(),
-    val drinks: MutableList<Drink> = mutableListOf(),
-    val desserts: MutableList<Dessert> = mutableListOf()
+    @JsonProperty("id") var id: String?,
+    @JsonProperty("burgers") val burgers: MutableList<Burger> = mutableListOf(),
+    @JsonProperty("drinks") val drinks: MutableList<Drink> = mutableListOf(),
+    @JsonProperty("desserts") val desserts: MutableList<Dessert> = mutableListOf()
 )
