99ea997082453005e1098704e34f74152557818e
Andrey <75383288+Andrey529@users.noreply.github.com>
2022-06-01 12:36:58 +0300

    fix(backend): fix jsonProperty property name

--- Backend/src/main/kotlin/products/desserts/DessertImpl.kt
+++ Backend/src/main/kotlin/products/desserts/DessertImpl.kt
@@ -3,21 +3,21 @@ package products.desserts
 import com.fasterxml.jackson.annotation.JsonProperty
 
 @kotlinx.serialization.Serializable
-data class Pie(@JsonProperty("id") override val price: Int, @JsonProperty("id") override val filling: Filling) : Dessert {
+data class Pie(@JsonProperty("price") override val price: Int, @JsonProperty("filling") override val filling: Filling) : Dessert {
     init {
         require(price > 0) { "Price must be greater than 0" }
     }
 }
 
 @kotlinx.serialization.Serializable
-data class Pancake(@JsonProperty("id") override val price: Int, @JsonProperty("id") override val filling: Filling) : Dessert {
+data class Pancake(@JsonProperty("price") override val price: Int, @JsonProperty("filling") override val filling: Filling) : Dessert {
     init {
         require(price > 0) { "Price must be greater than 0" }
     }
 }
 
 @kotlinx.serialization.Serializable
-data class Donut(@JsonProperty("id") override val price: Int, @JsonProperty("id") override val filling: Filling) : Dessert {
+data class Donut(@JsonProperty("price") override val price: Int, @JsonProperty("filling") override val filling: Filling) : Dessert {
     init {
         require(price > 0) { "Price must be greater than 0" }
     }
--- Backend/src/main/kotlin/products/drinks/DrinkImpl.kt
+++ Backend/src/main/kotlin/products/drinks/DrinkImpl.kt
@@ -3,28 +3,28 @@ package products.drinks
 import com.fasterxml.jackson.annotation.JsonProperty
 
 @kotlinx.serialization.Serializable
-data class Water(@JsonProperty("id") override val price: Int, @JsonProperty("id") override val volume: Volume) : Drink {
+data class Water(@JsonProperty("price") override val price: Int, @JsonProperty("volume") override val volume: Volume) : Drink {
     init {
         require(price > 0) { "Price must be greater than 0" }
     }
 }
 
 @kotlinx.serialization.Serializable
-data class Cola(@JsonProperty("id") override val price: Int, @JsonProperty("id") override val volume: Volume) : Drink {
+data class Cola(@JsonProperty("price") override val price: Int, @JsonProperty("volume") override val volume: Volume) : Drink {
     init {
         require(price > 0) { "Price must be greater than 0" }
     }
 }
 
 @kotlinx.serialization.Serializable
-data class Cherry(@JsonProperty("id") override val price: Int, @JsonProperty("id") override val volume: Volume) : Drink {
+data class Cherry(@JsonProperty("price") override val price: Int, @JsonProperty("volume") override val volume: Volume) : Drink {
     init {
         require(price > 0) { "Price must be greater than 0" }
     }
 }
 
 @kotlinx.serialization.Serializable
-data class Fanta(@JsonProperty("id") override val price: Int, @JsonProperty("id") override val volume: Volume) : Drink {
+data class Fanta(@JsonProperty("price") override val price: Int, @JsonProperty("volume") override val volume: Volume) : Drink {
     init {
         require(price > 0) { "Price must be greater than 0" }
     }
